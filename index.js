a9197aedd7f5967e83f8acc155eba1ca96a404c5
mau-io <maurice222@outlook.com>
2020-08-28 00:02:54 -0500

    Updated _log function

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const { readdirSync, statSync } = require('fs');
-const { join , basename } = require('path');
+const { join , basename, extname } = require('path');
 
+const ANSI = {
+  reset: '\033[0m',
+  red:   '\033[31m',
+  green: '\033[32m',
+  blue:  '\033[34m',
+}
 module.exports = class Loader {
-  constructor() {
-    this._modules = new Map();
+
+  constructor({verbose = false } = {}) {
+    this._verbose = verbose;
+    this._modules;
   }
   
-  init(path) {
-
-    let folderList = this._getFolders(path);
+  path(path) {
+    this._modules = new Map();
+    const folderList = this._getFolders(path);
 
     folderList.forEach(folder => {
-      console.log(`LOAD MODULE FOR: ${folder}`);
-      let object = this._loader(path, folder); 
+      this._log(`BUILD MODULE: ${folder}`);
+      const object = this._loader(path, folder);
       this._modules.set(folder, object);
     });
 
     if(!folderList.length) {
-      return this._loader(path, '');
+      const object = this._loader(path, '');
+      this._modules.set('madres', object);
     }
+
     return this._modules;
+  }
+
+  get(name) {
+    return this._modules.get(name);
   }
 
   _loader(path, name, key = '') {
@@ -31,23 +45,31 @@ module.exports = class Loader {
       const completePath = join(path, name, key, file);
       
       if(statSync(completePath).isFile()) {
+    
+        if(extname(file) === '.js') {
+          this._log(`LOAD: ${name}/${key}/${file}`, 'blue', 3);
+          const prop = basename(completePath, '.js');
+          object[prop] = require(completePath);
+        }
         
-        const key = basename(completePath, '.js');
-        object[key] = require(completePath);
-
-      }else {
+      } 
+      else {
        object[file] = this._loader(path, name, file);
       }
       
     });
+
     return object;
   }
   
-  get(name) {
-    return this._modules.get(name);
-  }
-
   _getFolders(path) { 
     return readdirSync(path).filter(f => statSync(join(path, f)).isDirectory());
   }
+
+  _log(mesage, color = 'green', pad = 0) {
+    if(this._verbose) {
+      console.log(`${ANSI[color]} ${'>'.padStart(pad, '-')} ${mesage} ${ANSI.reset}`);
+    }
+  }
+
 }
