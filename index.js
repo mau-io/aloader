8ff16662577fa034d51a40b838c779f875d97de6
mau-io <maurice222@outlook.com>
2020-08-30 22:34:00 -0500

    Fixed load files and folders function

--- index.js
+++ index.js
@@ -8,79 +8,68 @@ const ANSI = {
   blue:  '\033[34m',
 }
 
-module.exports = class Loader {
+const _getElements = (path) => { 
+  return readdirSync(path)
+    .map(
+      (name) => ({
+        name,
+        isFile: statSync(join(path, name)).isFile()
+      })
+    );
+}
 
-  constructor({verbose = false } = {}) {
-    this._verbose = verbose;
-    this._modules;
+module.exports = ({ path, verbose = false }) => {
+
+  const modules = new Map();
+  const elements = _getElements(path);
+
+  const _log = (mesage, color = 'green', pad = 0) => {
+    if(verbose) {
+      console.log(`${ANSI[color]} ${'>'.padStart(pad, '-')} ${mesage} ${ANSI.reset}`);
+    }
   }
   
-  path(path) {
-    this._modules = new Map();
-    const elements = this._getElements(path);
-
-    for(const element of elements) {
-   
-      if(element.isFile) {
-        this._log(`LOAD FILE: /${element.name}`, 'blue', 4);
-        const completePath = join(path, element.name);
-        const prop = basename(completePath, '.js');
-        const object = require(completePath);
-        this._modules.set(prop, object);
-      }
-      else {
-        this._log(`LOAD SUBMODULE: ${element.name}`);
-        const object = this._loader(path, element.name);
-        this._modules.set(element.name, object);
-      }    
-    }
-    return this._modules;
-  }
-
-  get(name) {
-    return this._modules.get(name);
-  }
-
-  _loader(path, name, key = '') {
+  const _loader = (path, name, key = '') => {
     const files = readdirSync(join(path, name, key));
-
+  
     let object = {};
-
+  
     files.forEach(file => {
       const completePath = join(path, name, key, file);
       
       if(statSync(completePath).isFile()) {
     
         if(extname(file) === '.js') {
-          this._log(`LOAD FILE: ${name}/${key}/${file}`, 'blue', 4);
+          _log(`LOAD FILE: ${name}/${key}/${file}`, 'blue', 4);
           const prop = basename(completePath, '.js');
           object[prop] = require(completePath);
         }
         
       } 
       else {
-       object[file] = this._loader(path, name, file);
+       object[file] = _loader(path, name, file);
       }
       
     });
-
+  
     return object;
   }
-  
-  _getElements(path) { 
-    return readdirSync(path)
-      .map(
-        (name) => ({
-          name,
-          isFile: statSync(join(path, name)).isFile()
-        })
-      );
-  }
-
-  _log(mesage, color = 'green', pad = 0) {
-    if(this._verbose) {
-      console.log(`${ANSI[color]} ${'>'.padStart(pad, '-')} ${mesage} ${ANSI.reset}`);
+ 
+  for(const element of elements) {
+ 
+    if(element.isFile) {
+      _log(`LOAD FILE: /${element.name}`, 'blue', 4);
+      const completePath = join(path, element.name);
+      const prop = basename(completePath, '.js');
+      const object = require(completePath);
+     modules.set(prop, object);
     }
+    else {
+      _log(`LOAD SUBMODULE: ${element.name}`);
+      const object = _loader(path, element.name);
+     modules.set(element.name, object);
+    }    
   }
+  return modules;
 
 }
